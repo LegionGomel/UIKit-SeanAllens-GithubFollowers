19d4e6cbf5f5967c5014b12d47e302444924d31c
Yuri Ershov <shtorm0606@gmail.com>
2022-01-25 17:55:15 +0300

    Add constraints to remaining elements

--- GithubFollowers/SearchVC.swift
+++ GithubFollowers/SearchVC.swift
@@ -20,6 +20,8 @@ class SearchVC: UIViewController {
         // set adaptive background(dark for dark mode end so on)
         view.backgroundColor = .systemBackground
         configureLogoImageView()
+        configureTextField()
+        configureCallToActionButton()
 
     }
     
@@ -48,6 +50,30 @@ class SearchVC: UIViewController {
             logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             logoImageView.heightAnchor.constraint(equalToConstant: 200),
             logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
+    }
+    
+    func configureTextField() {
+        view.addSubview(usernameTextField)
+        
+        NSLayoutConstraint.activate([
+            // pin top side of textField to bottom part of imageView at 48 points
+            usernameTextField.topAnchor.constraint(equalTo: logoImageView.bottomAnchor, constant: 48),
+            usernameTextField.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            // for trailing anchor we need to use negative points
+            usernameTextField.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            usernameTextField.heightAnchor.constraint(equalToConstant: 50)
+        ])
+    }
+    
+    func configureCallToActionButton() {
+        view.addSubview(callToActionButton)
+        
+        NSLayoutConstraint.activate([
+            callToActionButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -50),
+            callToActionButton.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
+            callToActionButton.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -50),
+            callToActionButton.heightAnchor.constraint(equalToConstant: 50)
         ])
     }
 }
