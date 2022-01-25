e7cd615e81d669f2959eb3ba5c4b5dc346a1d73e
Yuri Ershov <shtorm0606@gmail.com>
2022-01-25 17:37:34 +0300

    Add elements that will be onscreen. Hide nav bar from this screen. Add logoImageView to searchVC (to show image, as a subview, and configure it, set constraints)

--- GithubFollowers/SearchVC.swift
+++ GithubFollowers/SearchVC.swift
@@ -8,9 +8,46 @@
 import UIKit
 
 class SearchVC: UIViewController {
+    
+    
+    // create items that will be onscreen
+    let logoImageView = UIImageView()
+    let usernameTextField = GFTextField()
+    let callToActionButton = GFButton(backgroundColor: .systemGreen, title: "Get Followers")
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .systemPink
+        // set adaptive background(dark for dark mode end so on)
+        view.backgroundColor = .systemBackground
+        configureLogoImageView()
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        /*
+         Because of viewDidLoad isn't calls when screen go back,
+         we need to hide nav bar every time, when view shows on screen
+         */
+        navigationController?.isNavigationBarHidden = true
+    }
+    
+    func configureLogoImageView() {
+        /*
+         this line of code is like you drag imageView from library onto
+         storyboard and drop it in
+         */
+        view.addSubview(logoImageView)
+        // because this is not custom UIImageVIew (as GFButton for example), we need to add this code here.
+        logoImageView.translatesAutoresizingMaskIntoConstraints = false
+        logoImageView.image = UIImage(named: "gh-logo")! // stringly-typed. can have error here. We can have it as a constant
+        
+        // here we add constraints. safeAreaLayoutGuide represents safe area from storyboards
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 8),
+            logoImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            logoImageView.heightAnchor.constraint(equalToConstant: 200),
+            logoImageView.widthAnchor.constraint(equalToConstant: 200)
+        ])
     }
 }
