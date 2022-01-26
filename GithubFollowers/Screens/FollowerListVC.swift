d40630d5a034db1f62646ab48a35a5b529bbbca4
Yuri Ershov <shtorm0606@gmail.com>
2022-01-26 19:45:02 +0300

    add network request for get followers to followersVC

--- GithubFollowers/Screens/FollowerListVC.swift
+++ GithubFollowers/Screens/FollowerListVC.swift
@@ -9,12 +9,22 @@ import UIKit
 
 class FollowerListVC: UIViewController {
     
-    var username: String?
+    var username: String!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
         navigationController?.navigationBar.prefersLargeTitles = true
+        
+        NetworkManager.shared.getFollowers(for: username, page: 1) { followers, errorMessage in
+            guard let followers = followers else {
+                self.presentGFAlertOnMainThread(title: "Bad stuff happen", message: errorMessage!, buttonTitle: "Ok")
+                return
+            }
+            
+            print("followers.count = \(followers.count)")
+            print(followers)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
