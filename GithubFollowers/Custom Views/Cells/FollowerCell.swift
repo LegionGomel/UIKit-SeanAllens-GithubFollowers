28e0b0cfc516da362bfcb5eaf0e7566c659ad2f5
Yuri Ershov <shtorm0606@gmail.com>
2022-01-27 12:29:40 +0300

    Add elements to follower cell

--- GithubFollowers/Custom Views/Cells/FollowerCell.swift
+++ GithubFollowers/Custom Views/Cells/FollowerCell.swift
@@ -10,9 +10,34 @@ import UIKit
 class FollowerCell: UICollectionViewCell {
     // as in storyboard we use reusable ID, we need specify it here, in code
     static let reuseID = "FollowerCell"
-    
-    let avatarImageView = UIImageView()
+    let avatarImageView = GFAvatarImageView(frame: .zero)
     let usernameLabel = GFTitleLabel(textAlignment: .center, fontSize: 16)
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
     
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configure() {
+        addSubview(avatarImageView)
+        addSubview(usernameLabel)
+        
+        let padding: CGFloat = 8
+        
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: contentView.topAnchor, constant: padding),
+            avatarImageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            avatarImageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding),
+            avatarImageView.heightAnchor.constraint(equalTo: avatarImageView.widthAnchor),
+            
+            usernameLabel.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 12),
+            usernameLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: padding),
+            usernameLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -padding)
+            usernameLabel.heightAnchor.constraint(equalToConstant: 20)
+        ])
+    }
 }
