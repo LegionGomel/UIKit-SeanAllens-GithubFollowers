7fc747ac25e5c50aba328a322bde55a538c16ed5
Yuri Ershov <shtorm0606@gmail.com>
2022-01-28 10:37:50 +0300

    Add native caching for downloaded avatar images

--- GithubFollowers/Custom Views/GFAvatarImageView.swift
+++ GithubFollowers/Custom Views/GFAvatarImageView.swift
@@ -9,6 +9,7 @@ import UIKit
 
 class GFAvatarImageView: UIImageView {
     
+    let cache = NetworkManager.shared.cache
     let placeholderImage = UIImage(named: "avatar-placeholder")!
 
     override init(frame: CGRect) {
@@ -29,14 +30,24 @@ class GFAvatarImageView: UIImageView {
     }
     
     func downloadImage(from urlString: String) {
+        
+        let cacheKey = NSString(string: urlString)
+        
+        if let image = cache.object(forKey: NSString(string: cacheKey)) {
+            self.image = image
+            return
+        }
+        
         // we use placeholder image as the error and not handling an errors.
         guard let url = URL(string: urlString) else { return }
+        
         let task = URLSession.shared.dataTask(with: url) { [weak self] data, response, error in
             guard let self = self else { return }
             if let _ = error { return }
             guard let response = response as? HTTPURLResponse, response.statusCode == 200 else { return }
             guard let data = data else { return }
             guard let image = UIImage(data: data) else { return }
+            self.cache.setObject(image, forKey: cacheKey)
             
             DispatchQueue.main.async {
                 self.image = image
