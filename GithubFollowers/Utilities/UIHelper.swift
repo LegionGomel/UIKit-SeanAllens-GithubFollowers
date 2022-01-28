37a2baa8753fc8290638ee045acc0ca7df5db9cd
Yuri Ershov <shtorm0606@gmail.com>
2022-01-28 09:55:19 +0300

    Make code refactoring with UIHelper struct, add [weak self] in network manager to control memory leaks

---
+++ GithubFollowers/Utilities/UIHelper.swift
@@ -0,0 +1,28 @@
+//
+//  UIHelper.swift
+//  GithubFollowers
+//
+//  Created by Yuri Ershov on 28.01.22.
+//
+
+import UIKit
+
+struct UIHelper {
+    
+    // layout tuning for collectionView
+    static func createThreeColumnFlowLayout(in view: UIView) -> UICollectionViewFlowLayout {
+        // total width of the screen
+        let width = view.bounds.width
+        // ui edge insets
+        let padding:CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2) // two minimum spacing for three columns
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        return flowLayout
+    }
+}
