5b57359098e13e50a8a61ceded18ffcfae7fba46
Yuri Ershov <shtorm0606@gmail.com>
2022-01-30 16:20:15 +0300

    Add custom init to parent view and create two subclass views for followers and profile info. Refactor folder structure a bit

---
+++ GithubFollowers/Custom Views/ViewControllers/ItemInfoVCs/GFFollowerItemVC.swift
@@ -0,0 +1,22 @@
+//
+//  GFFoolowerItemVC.swift
+//  GithubFollowers
+//
+//  Created by Yuri Ershov on 30.01.22.
+//
+
+import UIKit
+
+class GFFollowerItemVC: GFItemInfoVC {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        configureItems()
+    }
+    
+    private func configureItems() {
+        itemInfoViewOne.set(itemInfoType: .followers, withCount: user.followers)
+        itemInfoViewTwo.set(itemInfoType: .following, withCount: user.following)
+        actionButton.set(backgroundColor: .systemGreen, title: "Get Followers")
+    }
+}
